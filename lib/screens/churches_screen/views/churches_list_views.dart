417060385f47ba57e42152f25a8d7cb9300f8cc4
Jongui <joaogd53@gmail.com>
2018-12-20 19:00:05 +0100

    Final commit project 'Build a better Layout for the App V1'

--- lib/screens/churches_screen/views/churches_list_views.dart
+++ lib/screens/churches_screen/views/churches_list_views.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:prayer_app/components/cardviews/church_card_view.dart';
 import 'package:prayer_app/model/church.dart';
 import 'package:prayer_app/model/user.dart';
+import 'package:prayer_app/screens/loading_screen/loading_view.dart';
 import 'package:prayer_app/utils/church_http.dart';
 
-class ChurchesListView extends StatelessWidget{
-
+class ChurchesListView extends StatelessWidget {
   User user;
 
   ChurchesListView({@required this.user});
@@ -14,11 +14,9 @@ class ChurchesListView extends StatelessWidget{
   Widget build(BuildContext context) {
     return ChurchesListViewState(user);
   }
-
 }
 
 class ChurchesListViewState extends StatefulWidget {
-
   User user;
 
   ChurchesListViewState(this.user);
@@ -27,41 +25,41 @@ class ChurchesListViewState extends StatefulWidget {
   _ChurchesListViewState createState() => _ChurchesListViewState(user);
 }
 
-class _ChurchesListViewState extends State<ChurchesListViewState>{
-
+class _ChurchesListViewState extends State<ChurchesListViewState> {
   User user;
 
   _ChurchesListViewState(this.user);
-
+  Widget _view;
   List<Widget> _churchList = [];
 
   @override
   void initState() {
+    _view = LoadingView();
     _handleChurchesLoad();
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      padding: const EdgeInsets.all(20.0),
-      children: _churchList,
-    );
+    return _view;
   }
 
-  void _handleChurchesLoad() async{
+  void _handleChurchesLoad() async {
     List<Church> churches = await ChurchHttp().getChurches();
     setState(() {
       _churchList = [];
-      for(int i = 0; i < churches.length; i++) {
+      for (int i = 0; i < churches.length; i++) {
         Church church = churches.elementAt(i);
-        _churchList.add(ChurchCardView(church: church,
-          user: user,)
-        );
+        _churchList.add(ChurchCardView(
+          church: church,
+          user: user,
+        ));
       }
+      _view = ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(20.0),
+        children: _churchList,
+      );
     });
   }
-
 }
--- lib/screens/loading_screen/loading_view.dart
+++ lib/screens/loading_screen/loading_view.dart
@@ -5,7 +5,7 @@ class LoadingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.only(top: 24.0, left: 84.0),
+        alignment: Alignment(0.0, 0.0),
         child:CircularProgressIndicator()
     );
   }
